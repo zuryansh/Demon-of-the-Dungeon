14ab8eebd9150d7ef4a7d0d85dfb3ea4e84e4344
zuryansh <87305245+zuryansh@users.noreply.github.com>
2022-06-06 16:26:02 +0530

    Updated Ui

---
+++ Space Parasyte/Assets/ButtonAnimator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonAnimator : MonoBehaviour 
+{
+    //public Animator animator;
+
+    //public void OnPointerEnter(PointerEventData eventData)
+    //{
+    //    animator.SetTrigger("Highlighted");
+        
+    //}
+
+    //public void OnPointerExit(PointerEventData eventData)
+    //{
+    //    animator.SetTrigger("Normal");
+    //}
+}
